3918d7956cb5ec586e06ff442293502ce80c16bf
SAIFULLAH SHAIKH <ssaifullah272@gmail.com>
2020-11-18 15:50:01 +0500

    basic eact app 01

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Hello from './Hello'
+function App({name,age}) {
+  return <div> 
+    Hello from App.js updated {name} Age = {age -5}
+    <br/>
+    <Hello firstname={name}></Hello>
+    </div>
+}
+
+export default App;
